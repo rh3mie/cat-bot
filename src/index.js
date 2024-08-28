1c280a028e6fd3640e66763f59d691f62fc5d57a
rh3mie <114265500+rh3mie@users.noreply.github.com>
2024-08-28 18:34:09 -0400

    setting up bot w basic intents

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+console.log('TOKEN:', process.env.TOKEN);
+
+const { Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on("ready", (c) => {
+    console.log(`✅${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'meow :3') {
+        message.reply('meow :3');
+    }
+});
+
+client.login(process.env.TOKEN);
